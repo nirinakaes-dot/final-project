6ef41e69a0d15d081dbd766e7a0639422c33bcdd
Nina Adora <nirinakaes@gmai.com>
2026-02-12 19:24:34 +0300

    create a constructor for entry and adding an array to store the entries

--- java.js
+++ java.js
@@ -1,10 +1,23 @@
 
 // adding variables to access the textarea and button elements in the HTML document
-const entry = document.getElementById("Entry");
+const entryDiary = document.getElementById("Entry");
 const addEntry = document.getElementById("AddEntry"); 
 
 //assign an event listener to the addEntry button
 addEntry.addEventListener("click",function(){});
 
+//create a constructor for the entries//
+function Entry(Title,Date,Mood,Entry)
+
+{   this.Title=Title;
+    this.Date=Date;
+    this.Mood=Mood;
+    this.Entry=Entry;
+}
+
+//array to store the entries
+constnewEntry=[Title,Date,Mood,Entry];
+
+//
  
  
