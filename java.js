1bb16c59f34d65aa47cb3c287b7be4eacb245352
Nina Adora <nirinakaes@gmai.com>
2026-02-12 19:11:05 +0300

    adding an event listener to the add entry button

---
+++ java.js
@@ -0,0 +1,10 @@
+
+// adding variables to access the textarea and button elements in the HTML document
+const entry = document.getElementById("Entry");
+const addEntry = document.getElementById("AddEntry"); 
+
+//assign an event listener to the addEntry button
+addEntry.addEventListener("click",function(){});
+
+ 
+ 
